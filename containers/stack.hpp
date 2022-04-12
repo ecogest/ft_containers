ebe1972b9b5864d9486f96ec4f86837c5e9e7b3f
mjacq <mjacq@redoute.fr>
2022-04-12 14:41:23 +0200

    stack: constructors

--- containers/stack.hpp
+++ containers/stack.hpp
@@ -6,7 +6,7 @@
 /*   By: mjacq <mjacq@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/07 14:13:08 by mjacq             #+#    #+#             */
-/*   Updated: 2022/04/12 14:31:08 by mjacq            ###   ########.fr       */
+/*   Updated: 2022/04/12 14:40:19 by mjacq            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,54 +20,58 @@ namespace ft {
 template<class T, class Container = std::deque<T> >
 class stack {
 public:
-// ### Member types ////////////////////////////////////////////////////////////
-// 
-typedef 			Container					container_type;
-typedef typename	Container::value_type		value_type;
-typedef typename	Container::size_type		size_type;
-typedef typename	Container::reference		reference;
-typedef typename	Container::const_reference	const_reference;
+	// ### Member types ////////////////////////////////////////////////////////////
+	// 
+	typedef 			Container					container_type;
+	typedef typename	Container::value_type		value_type;
+	typedef typename	Container::size_type		size_type;
+	typedef typename	Container::reference		reference;
+	typedef typename	Container::const_reference	const_reference;
 
 
-// ### Member functions
-// 
-// [(constructor)](https://en.cppreference.com/w/cpp/container/stack/stack "cpp/container/stack/stack")
-// constructs the `stack`  
-// 
-// [(destructor)](https://en.cppreference.com/w/cpp/container/stack/~stack "cpp/container/stack/~stack")
-// destructs the `stack`  
-// 
-// [operator=](https://en.cppreference.com/w/cpp/container/stack/operator%3D "cpp/container/stack/operator=")
-// assigns values to the container adaptor  
+	// ### Member functions
+	// 
+	// [(constructor)](https://en.cppreference.com/w/cpp/container/stack/stack "cpp/container/stack/stack")
+	// constructs the `stack`  
+	// 2) Copy-constructs the underlying container c with the contents of cont. 
+	// 4) Copy constructor. The adaptor is copy-constructed with the contents of other.c.
+	explicit stack(const Container& cont = Container()) : c(cont)    { } // (2)
+	stack( const stack& other )                         : c(other.c) { } // (4)
+	// [(destructor)](https://en.cppreference.com/w/cpp/container/stack/~stack "cpp/container/stack/~stack")
+	// destructs the `stack`  
+	// 
+	// [operator=](https://en.cppreference.com/w/cpp/container/stack/operator%3D "cpp/container/stack/operator=")
+	// assigns values to the container adaptor  
 
 
-// ##### Element access
-// 
-// [top](https://en.cppreference.com/w/cpp/container/stack/top "cpp/container/stack/top")
-// accesses the top element  
+	// ##### Element access
+	// 
+	// [top](https://en.cppreference.com/w/cpp/container/stack/top "cpp/container/stack/top")
+	// accesses the top element  
 
 
-// ##### Capacity
-// [empty](https://en.cppreference.com/w/cpp/container/stack/empty "cpp/container/stack/empty")
-// checks whether the underlying container is empty  
-// 
-// [size](https://en.cppreference.com/w/cpp/container/stack/size "cpp/container/stack/size")
-// returns the number of elements  
+	// ##### Capacity
+	// [empty](https://en.cppreference.com/w/cpp/container/stack/empty "cpp/container/stack/empty")
+	// checks whether the underlying container is empty  
+	// 
+	// [size](https://en.cppreference.com/w/cpp/container/stack/size "cpp/container/stack/size")
+	// returns the number of elements  
 
 
-// ##### Modifiers
-// 
-// [push](https://en.cppreference.com/w/cpp/container/stack/push "cpp/container/stack/push")
-// inserts element at the top  
+	// ##### Modifiers
+	// 
+	// [push](https://en.cppreference.com/w/cpp/container/stack/push "cpp/container/stack/push")
+	// inserts element at the top  
 
-// [pop](https://en.cppreference.com/w/cpp/container/stack/pop "cpp/container/stack/pop")
-// removes the top element  
+	// [pop](https://en.cppreference.com/w/cpp/container/stack/pop "cpp/container/stack/pop")
+	// removes the top element  
 
 
-// ### Member objects
-// 
-// Container c
-// the underlying container  
+	// ### Member objects
+	// 
+	// Container c
+	// the underlying container  
+	container_type	c;
 
 private:
 };
--- tests/stack_test.cpp
+++ tests/stack_test.cpp
@@ -1,5 +1,9 @@
 #include "tests.hpp"
+#include "stack.hpp"
+#include <stack>
 
 void test_stack() {
+	NS::stack<int>	s;
+	NS::stack<int>	t(s);
 
 }
